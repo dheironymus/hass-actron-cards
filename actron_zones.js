34ae17195c93943e371af7e2f9bb59f753c13015
dheironymus <dheironymus@users.noreply.github.com>
2023-10-14 22:05:14 +1000

    Create actron_zones.js

---
+++ actron_zones.js
@@ -0,0 +1,35 @@
+class ActronZonesCard extends HTMLElement {
+  // Whenever the state changes, a new `hass` object is set. Use this to
+  // update your content.
+  set hass(hass) {
+    // Initialize the content if it's not there yet.
+    if (!this.content) {
+      this.innerHTML = `
+        <ha-card header="Zones">
+          <div class="card-content"></div>
+        </ha-card>
+      `;
+      this.content = this.querySelector("div");
+    }
+
+    const entityId = this.config.entity;
+    const state = hass.states[entityId];
+    const stateStr = state ? state.state : "unavailable";
+
+    this.content.innerHTML = `
+      The state of ${entityId} is ${stateStr}!
+      <br><br>
+    `;
+  }
+
+  // The user supplied configuration. Throw an exception and Home Assistant
+  // will render an error card.
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error("You need to define an entity");
+    }
+    this.config = config;
+  }
+}
+
+customElements.define("actron-zones-card", ActronZonesCard);
